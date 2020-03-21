ee0e5e208323308445c298b5443c96fa56eddd2f
onurmatik <omat@gezgin.com>
2020-03-21 12:52:36 +0300

    updated settings

--- solfasol/settings.py
+++ solfasol/settings.py
@@ -51,7 +51,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'solfasol.context_processors.categories',
+                'solfasol.context_processors.generic',
             ],
         },
     },
