c462c57cb864489363876e1e13eca57911ccc3e1
onurmatik <onurmatik@gmail.com>
2020-04-26 21:52:10 +0300

    admin QR code

--- solfasol/content/admin.py
+++ solfasol/content/admin.py
@@ -1,3 +1,8 @@
+from io import BytesIO
+import qrcode
+import qrcode.image.svg
+from slugify import slugify
+from django.http import HttpResponse
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from .models import Content, Article, Video, Contributor, Tag, Category, Series
@@ -12,10 +17,13 @@ class ContentAdmin(admin.ModelAdmin):
     list_display = ['title', 'added', 'modified', 'publish', 'published_by', 'featured', 'pinned', 'view_count']
     search_fields = ['title', 'summary', 'tags__name']
     exclude = ['published_by']
-    list_editable = ['publish', 'featured']
+    list_editable = ['publish', 'featured', 'category']
     list_filter = ['publish', 'added', 'modified', 'featured']
-    autocomplete_fields = ['tags', 'category', 'related_content']
-    actions = ['publish']
+#    autocomplete_fields = ['tags', 'category', 'related_content']
+    actions = ['publish', 'get_qr']
+    formfield_overrides = {
+        MartorField: {'widget': AdminMartorWidget},
+    }
 
     def publish(self, request, queryset):
         for article in queryset:
@@ -25,15 +33,38 @@ class ContentAdmin(admin.ModelAdmin):
                 article.save()
     publish.short_description = _('Publish')
 
+    def get_qr(self, request, queryset):
+        factory = qrcode.image.svg.SvgFragmentImage
+        for content in queryset:
+            qr = qrcode.QRCode(
+                version=None,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=6,
+                border=1,
+                image_factory=factory,
+            )
+            qr.add_data(
+                'http://solfasol.tv%s' % content.get_absolute_url()
+            )
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="black", back_color="white")
+            img_buffer = BytesIO()
+            img.save(img_buffer)
+            img_buffer.seek(0)
+            response = HttpResponse(
+                img_buffer.read(),
+                content_type='image/svg+xml',
+            )
+            response['Content-Disposition'] = 'attachment; filename="QR-%s.svg"' % slugify(content.slug[:10])
+            break
+        return response
+    get_qr.short_description = _('Get QR code')
+
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
+class ArticleAdmin(ContentAdmin):
     list_display = ['title', 'author', 'added', 'category', 'publish', 'published_by', 'featured', 'pinned', 'view_count']
     search_fields = ['title', 'author', 'summary', 'tags__name']
-    exclude = ['published_by']
-    list_editable = ['publish', 'featured', 'category']
-    list_filter = ['publish', 'added', 'modified', 'featured']
     #autocomplete_fields = [
     filter_horizontal = [
         #'series',
@@ -43,28 +74,12 @@ class ArticleAdmin(admin.ModelAdmin):
         #'category',
         #'related_content'
     ]
-    actions = ['publish']
-    formfield_overrides = {
-        MartorField: {'widget': AdminMartorWidget},
-    }
-
-    def publish(self, request, queryset):
-        for article in queryset:
-            if not article.publish:
-                article.publish = True
-                article.published_by = request.user
-                article.save()
-    publish.short_description = _('Publish')
 
 
 @admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
+class VideoAdmin(ContentAdmin):
     list_display = ['title', 'host', 'added', 'category', 'publish', 'published_by', 'featured', 'pinned', 'view_count']
     search_fields = ['title', 'host', 'guests', 'summary', 'tags__name']
-    exclude = ['published_by']
-    list_editable = ['publish', 'featured', 'category']
-    list_filter = ['publish', 'added', 'modified', 'featured']
     #autocomplete_fields = [
     filter_horizontal = [
         #'series',
@@ -74,18 +89,6 @@ class VideoAdmin(admin.ModelAdmin):
         #'category',
         #'related_content'
     ]
-    actions = ['publish']
-    formfield_overrides = {
-        MartorField: {'widget': AdminMartorWidget},
-    }
-
-    def publish(self, request, queryset):
-        for article in queryset:
-            if not article.publish:
-                article.publish = True
-                article.published_by = request.user
-                article.save()
-    publish.short_description = _('Publish')
 
 
 @admin.register(Contributor)
