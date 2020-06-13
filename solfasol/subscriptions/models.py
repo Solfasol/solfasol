328139c5fcda163b262bef3d5b03f95928d56cc9
onurmatik <onurmatik@gmail.com>
2020-06-13 15:17:54 +0300

    sozel duzeltmeler

--- solfasol/subscriptions/models.py
+++ solfasol/subscriptions/models.py
@@ -11,10 +11,10 @@ class Subscription(models.Model):
         help_text="Dijital dışı abonelikler için gerekli"
     )
     type = models.CharField("Abonelik türü", max_length=10, default='destekci', choices=(
-        ('dijital', "Dijital abonelik - 50 TL (Solfasol'unuz her ay elektronik posta kutunuzda!)"),
-        ('yillik', "Yıllık abonelik - 100 TL (Solfasol'unuz her ay adresinize posta yoluyla ulaştırılır!)"),
-        ('destekci', "Destekçi abonelik - 200 TL (Solfasol'un Ankara'nın farklı kesimlerine ulaşmasına destek olun! "
-                     "Ayrıca Solfasol Ankara Gezilerinde misafirimiz olun."),
+        ('dijital', "Dijital abonelik - 50 TL (Solfasol her ay elektronik posta kutunuzda!)"),
+        ('yillik', "Yıllık abonelik - 100 TL (Solfasol her ay adresinize posta yoluyla ulaştırılır!)"),
+        ('destekci', "Destekçi abonelik - 200 TL (Hem daha çok kişiye ulaşmaımıza yardımcı olun "
+                     "hem de Solfasol Ankara Gezilerinde misafirimiz olun."),
         ('yurtdisi', "Yurtdışı abonelik - 200 TL (Uzaktayım demeyin, Solfasol dünyanın her yerine ulaşıyor!)"),
     ))
     phone = models.CharField("Telefon numaranız", max_length=20)
