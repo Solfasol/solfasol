a444cdc6b2ea200dc6de234aa2d617d682d66df2
onurmatik <onurmatik@gmail.com>
2020-08-16 12:03:50 +0300

    populer icerigi son 30 gun ile sinirla

--- solfasol/context_processors.py
+++ solfasol/context_processors.py
@@ -1,8 +1,14 @@
+from datetime import timedelta
+from django.utils import timezone
 from solfasol.content.models import Category, Content
 
 
 def generic(request):
     return {
         'categories': Category.objects.all(),
-        'popular_content': Content.objects.filter(publish=True).order_by('-view_count')[:6],
+        'popular_content': Content.objects.filter(
+            publish=True
+        ).filter(
+            added__gt=timezone.now()-timedelta(days=30)
+        ).order_by('-view_count')[:6],
     }
