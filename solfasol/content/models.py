e57e19b0351d25d324d678e278b70c74ce2988ad
onurmatik <onurmatik@gmail.com>
2020-10-12 14:02:41 +0300

    categories

--- solfasol/content/models.py
+++ solfasol/content/models.py
@@ -246,7 +246,7 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('content_category_list', kwargs={'category': self.slug})
 
-    @property
+    @cached_property
     def content(self):
         return self.content_set.filter(publish=True)
 
