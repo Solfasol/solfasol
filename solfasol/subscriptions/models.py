fb1b62752e630e6e5963125169421bb6b48ec5c5
onurmatik <onurmatik@gmail.com>
2021-01-13 00:40:08 +0300

    abonelik guncellendi

--- solfasol/subscriptions/models.py
+++ solfasol/subscriptions/models.py
@@ -11,11 +11,12 @@ class Subscription(models.Model):
         help_text="Dijital dışı abonelikler için gerekli"
     )
     type = models.CharField("Abonelik türü", max_length=10, default='destekci', choices=(
-        ('dijital', "Dijital abonelik - 50 TL (Solfasol her ay elektronik posta kutunuzda!)"),
-        ('yillik', "Yıllık abonelik - 100 TL (Solfasol her ay adresinize posta yoluyla ulaştırılır!)"),
-        ('destekci', "Destekçi abonelik - 200 TL (Hem daha çok kişiye ulaşmaımıza yardımcı olun "
-                     "hem de Solfasol Ankara Gezilerinde misafirimiz olun!)"),
-        ('yurtdisi', "Yurtdışı abonelik - 200 TL (Uzaktayım demeyin, Solfasol dünyanın her yerine ulaşıyor!)"),
+        ('dijital', "Dijital abonelik - 100 TL"),
+        ('yillik', "Yıllık abonelik - 150 TL"),
+        ('destekci', "Destekçi abonelik - 300 TL"),
+        ('yurtdisi', "Yurtdışı abonelik - 300 TL"),
+        ('duble', "Duble Destekçi abonelik - 600 TL"),
+        ('yasasin', "Yaşasın SOLFASOL! aboneliği - 1000 TL"),
     ))
     renewal = models.BooleanField(_('subscription status'), default=False,
         choices=((False, _('New subscription')),(True, _('Renewal')),)
