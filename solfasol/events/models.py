4663e8d3b920e76b13c182fd29d24e20074b1124
onurmatik <onurmatik@gmail.com>
2021-04-10 19:45:55 +0300

    Etkinlik guncellemeleri

--- solfasol/events/models.py
+++ solfasol/events/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class EventType(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField("Tip", max_length=50)
 
     def __str__(self):
         return self.name
