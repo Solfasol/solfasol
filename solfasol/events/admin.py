b9faadea333207901e6a84d036731f9e9c07d6fc
onurmatik <onurmatik@gmail.com>
2021-04-10 19:48:10 +0300

    Etkinlik tipi admin

--- solfasol/events/admin.py
+++ solfasol/events/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from solfasol.events.models import Event
+from solfasol.events.models import Event, EventType
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ['title', 'start', 'end', 'online', 'active']
     list_editable = ['active']
+
+
+@admin.register(EventType)
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
